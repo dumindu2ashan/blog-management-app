bbee2bb6c60a7e62575da8e885a979f014632663
dumindu2ashan <dumindu2ashan@gmail.com>
2023-10-17 16:05:01 +0530

    laravel authentication added.

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
+Route::group(['middleware' => ['auth','verified']], function() {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+});
